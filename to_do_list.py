0ef90c63b0106fc0bd8411e38b889d0235bfa78e
tuanhnguyener <155795969+tuanhnguyener@users.noreply.github.com>
2025-10-18 19:28:44 -0500

    Add files via upload

--- to_do_list.py
+++ to_do_list.py
@@ -1,22 +1,22 @@
 
-
 class TodoList:
     def __init__(self):
         self.to_do_list = []
-    
 
     def run(self):
         while True:
+            #print menu for user to choose
             print("To-Do List Menu")
             print("1. View all tasks")
             print("2. Add a task")
             print("3. Complete a task")
             print("4. Delete a task")
             print("5. Clear all tasks")
-            print ("6. Exit")
 
-            choice = input("Enter your choice (1-6): ")
+            #ask user what option they want
+            choice = input("Enter your choice (1-5): ")
 
+            #based on what the user chooses, it will run the respective functions
             if choice == "1":
                 self.view_tasks()
 
@@ -32,15 +32,13 @@ class TodoList:
             elif choice == '5':
                 self.to_do_list.clear()
                 print("All tasks cleared")
-            elif choice == '6':
-                print("Exiting To-Do List.")
-                break
+
             else:
                 print("Invalid choice. Please enter 1-5.")
 
-
+    #this function will add task to to-do list
     def add_task(self):
-        #asking the user to input a task
+        #ask the user to input a task
         task = {
             'number': len(self.to_do_list) + 1,
             'name': input("Enter task: "),
@@ -53,22 +51,24 @@ class TodoList:
         else: 
             print(f"That task already exists.")
 
-
+    #this function will allow the user to view the to-do list
     def view_tasks(self):
         if not self.to_do_list:
             print("There are no tasks yet. Add one to get started.")
             return
         
+        #mark task appropriately based on status (✓ = completed, ○ = not completed)
         for task in self.to_do_list:
             status = "✓" if task['completed'] else "○"
 
+            #if task is completed, it will have a strikethrough for the task
             task_name = task['name']
             if task['completed']:
                 task_name = f"\033[9m{task_name}\033[0m"  # Strikethrough
 
             print(f"{status} [{task['number']}] {task_name}")
 
-
+    #this function will mark a task as complete
     def complete_task(self):
         self.view_tasks()
         task_number = int(input(f"Enter task number completed: ")) 
@@ -80,7 +80,7 @@ class TodoList:
             
         print((f"Task {task_number} not found."))
 
-
+    #this function will remove a task from the to-do list
     def remove_task(self):
         self.view_tasks()
         try:
@@ -94,7 +94,7 @@ class TodoList:
         except ValueError:
             print(f"Please enter a valid task number.")   
     
-
-# if __name__ == "__main__":
-#     todo_list = TodoList()
-#     todo_list.run()
+#runs the program
+if __name__ == "__main__":
+    todo_list = TodoList()
+    todo_list.run()
