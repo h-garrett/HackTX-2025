49db7cfa3de4505d66d926d066de4ba19af1cf0f
JaxPet0125 <jjpetty2006@gmail.com>
2025-10-19 00:35:49 -0500

    Add files via upload

--- Constellation Hackathon Fall Main 2025.py
+++ Constellation Hackathon Fall Main 2025.py
@@ -11,14 +11,19 @@ def main():
     star1 = Star(Point(4,0.22), "test 1")
     star2 = Star(Point(9,-1.4794129133224488), "data 2'")
     star3 = Star(Point( 1,0.32134079933166506), "data 3")
+    star4 = Star(Point( 0,0), "data 4")
     Constellation1 = Constellation()
     Constellation1.set_name("My First Constellation")
     Constellation1.add_star(star1)
     Constellation1.add_star(star2)
     Constellation1.add_star(star3)
+    Constellation1.add_star(star4)
+
     Constellation1.get_star(0).light_star()
     os.makedirs("data", exist_ok=True)
 
+    Constellation2 = Constellation().load_from_json("data/test.json")
+    print(Constellation2)
     Constellation1.save_to_json("data/test.json")
     
 
